7ca4d8d5e6fc5a0fbf4ea0ce3a04602787f10d0c
Hookkn2617 <144010603+Hookkn2617@users.noreply.github.com>
2023-11-23 13:51:21 +0700

    Add files via upload

---
+++ songuyenti.py
@@ -0,0 +1,26 @@
+snt =[]
+
+def timsonguyento(so):
+    for i in range(0, so+1):
+        if(checksonguyento(i) == True):
+            snt.append(i)
+
+def checksonguyento(so):
+    if(so < 2):
+        return False
+
+    for i in range(2, so):
+        if(so % i == 0):
+            return False
+    return True
+
+n = int(input())
+
+timsonguyento(n)
+
+if(len(snt) <= 0):
+    print("ko co snt nao nho hon ", n)
+else:
+    print("cac snt nho hon n la: ",end="")
+    for i in snt:
+        print(i,end=", ")
